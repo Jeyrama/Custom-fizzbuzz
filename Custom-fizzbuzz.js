62ef0102ef3ac34955c1751e6b734a9bdf8107f1
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-21 20:29:47 -0700

    Update Custom-fizzbuzz.js

--- Custom-fizzbuzz.js
+++ Custom-fizzbuzz.js
@@ -46,3 +46,12 @@ let fizzBuzzCustom = function(stringOne, stringTwo, numOne, numTwo) {
 };
 
 // or
+
+const fizzBuzzCustomArray = (fizz='Fizz', buzz='Buzz', num1=3, num2=5) => {
+  return Array.from(new Array(100), (nums, i) => i + 1).map((num) => {
+    if (!(num % (num1 * num2))) return fizz + buzz;
+    else if (!(num % num1)) return fizz;
+    else if (!(num % num2)) return buzz;
+    else return num;
+  })
+}
